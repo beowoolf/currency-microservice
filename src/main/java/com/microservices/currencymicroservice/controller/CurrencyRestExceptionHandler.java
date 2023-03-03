5dfe237c636c63addf6d5519083cbef3f8365cb1
beowoolf <beowoolf@users.noreply.github.com>
2023-03-03 18:50:27 +0100

    Replacing the unnecessary self-written CurrencyApiError class with the existing Spring Boot ResponseStatusException class.

--- src/main/java/com/microservices/currencymicroservice/controller/CurrencyRestExceptionHandler.java
+++ src/main/java/com/microservices/currencymicroservice/controller/CurrencyRestExceptionHandler.java
@@ -1,12 +1,12 @@
 package com.microservices.currencymicroservice.controller;
 
 import com.microservices.currencymicroservice.exception.CurrencyNotFoundException;
-import com.microservices.currencymicroservice.model.CurrencyApiError;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @Slf4j
@@ -14,10 +14,10 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 public class CurrencyRestExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler({CurrencyNotFoundException.class})
-    public ResponseEntity<CurrencyApiError> handleCurrencyNotFoundException(CurrencyNotFoundException ex) {
+    public ResponseEntity<ResponseStatusException> handleCurrencyNotFoundException(CurrencyNotFoundException ex) {
         String message = ex.getMessage();
         log.error("ErrorMessage: {} , exceptionBody: {}", message, ex);
-        final CurrencyApiError currencyApiError = new CurrencyApiError(HttpStatus.BAD_REQUEST, message, ex);
+        final ResponseStatusException currencyApiError = new ResponseStatusException(HttpStatus.BAD_REQUEST, message, ex);
         return new ResponseEntity<>(currencyApiError, currencyApiError.getStatus());
     }
 
